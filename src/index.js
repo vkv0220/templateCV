2921a6fa6919fc8eb66bc17cb23285cf63ec4c03
vkv0220 <36685410+vkv0220@users.noreply.github.com>
2018-11-01 10:40:50 +0700

    webpack version

---
+++ src/index.js
@@ -0,0 +1,8 @@
+//import $ from 'jquery';
+
+// $('.title').html('Some text!!!')
+//import './css/my-styles.less';
+// document.querySelector('body').classList.add('with-styles');
+
+import './css/styles.less';
+import './css/styles.styl';
